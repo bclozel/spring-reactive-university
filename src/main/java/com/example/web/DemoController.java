7ee1022f8b3a4279d7ab08b6ae5ebfc6d5c1b8d3
Brian Clozel <bclozel@pivotal.io>
2016-11-06 09:40:54 +0100

    Add demo web endpoints
    First, let's create a few web endpoints to understand how reactive types
    can be used in a web application:
    
    1. Synchronous return types can be still used and will be wrapped by the
    framework. This is allowed as long as the controller method is not
    blocking.
    
    2. With the use of `Mono.never()`, we can see that the reactive type not
    only controls the HTTP response body, but also when that response is
    written.
    
    3. The HTTP contract is not based on Servlet here, but rather on the
    `ServerWebExchange` contract and `DataBuffer`.
    
    4. Errors can also flow through the reactive pipeline and those should
    be handled by application code or by the framework itself.

--- src/main/java/com/example/web/DemoController.java
+++ src/main/java/com/example/web/DemoController.java
@@ -1,7 +1,20 @@
 package com.example.web;
 
+import java.nio.charset.StandardCharsets;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 
 /**
  * @author Brian Clozel
@@ -9,5 +22,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+
+	// You should create and share a single instance, or even preferrably
+	// get the the factory from the underlying container
+	private DataBufferFactory dataBufferFactory = new DefaultDataBufferFactory();
+
+	@RequestMapping("/test")
+	public String test() {
+		return "This is a test";
+	}
+
+	@RequestMapping("/hello")
+	public Mono<String> hello(@RequestParam String name) {
+		return Mono.just("Hello, " + name + "!");
+	}
+
+	@RequestMapping("/waitforit")
+	public Mono<String> waiting() {
+		return Mono.never();
+	}
+
+	@RequestMapping("/exchange")
+	public Mono<Void> exchange(ServerWebExchange exchange) {
+		ServerHttpResponse response = exchange.getResponse();
+		response.setStatusCode(HttpStatus.OK);
+		response.getHeaders().setContentType(MediaType.TEXT_PLAIN);
+		DataBuffer buf = dataBufferFactory.wrap("Hello from exchange".getBytes(StandardCharsets.UTF_8));
+		return response.writeWith(Flux.just(buf));
+	}
+
+	@RequestMapping("/error")
+	public Mono<String> error() {
+		return Mono.error(new IllegalArgumentException("My custom error message"));
+	}
 
 }
