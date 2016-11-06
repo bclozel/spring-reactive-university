b8430cd325a028da76d92943d8d8372b0426c227
Brian Clozel <bclozel@pivotal.io>
2016-11-06 10:07:20 +0100

    Use a reactive Spring Data repository
    With the right database driver and repository API, we can leverage our
    datastore in our reactive pipeline.
    
    The `CommandlineRunner` interface requires a blocking operation (i.e. it
    should complete before the method returns). Since this is not done while
    handling requests and only at startup, we can safely block here.
    
    We can compose our pipeline with operators in order to handle exceptions
    or empty values.

--- src/main/java/com/example/SpringReactiveDashboardApplication.java
+++ src/main/java/com/example/SpringReactiveDashboardApplication.java
@@ -1,5 +1,10 @@
 package com.example;
 
+import com.example.dashboard.ReactorPerson;
+import com.example.dashboard.ReactorPersonRepository;
+import reactor.core.publisher.Flux;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +12,7 @@ import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfigurat
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties({DashboardProperties.class, MongoProperties.class})
@@ -15,6 +21,24 @@ public class SpringReactiveDashboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactiveDashboardApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initDatabase(ReactorPersonRepository repository) {
+
+		Flux<ReactorPerson> people = Flux.just(
+						new ReactorPerson("smaldini", "Stephane Maldini"),
+						new ReactorPerson("simonbasle", "Simon Basle"),
+						new ReactorPerson("akarnokd", "David Karnok"),
+						new ReactorPerson("rstoya05", "Rossen Stoyanchev"),
+						new ReactorPerson("sdeleuze", "Sebastien Deleuze"),
+						new ReactorPerson("poutsma", "Arjen Poutsma"),
+						new ReactorPerson("bclozel", "Brian Clozel")
+				);
+
+		return args -> {
+			repository.deleteAll().thenMany(repository.save(people)).blockLast();
+		};
 	}
 
 }
--- src/main/java/com/example/web/DashboardController.java
+++ src/main/java/com/example/web/DashboardController.java
@@ -1,11 +1,20 @@
 package com.example.web;
 
 import com.example.dashboard.DashboardService;
+import com.example.dashboard.ReactorPerson;
+import com.example.dashboard.ReactorPersonNotFoundException;
 import com.example.dashboard.ReactorPersonRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author Brian Clozel
@@ -26,6 +35,24 @@ public class DashboardController {
 	@GetMapping("/")
 	public String home() {
 		return "home";
+	}
+
+	@GetMapping("/reactor/people")
+	@ResponseBody
+	public Flux<ReactorPerson> findReactorPeople() {
+		return this.repository.findAll();
+	}
+
+	@GetMapping("/reactor/people/{id}")
+	@ResponseBody
+	public Mono<ReactorPerson> findReactorPerson(@PathVariable String id) {
+		return this.repository.findOne(id)
+				.otherwiseIfEmpty(Mono.error(new ReactorPersonNotFoundException(id)));
+	}
+
+	@ExceptionHandler
+	public ResponseEntity handleNotFoundException(ReactorPersonNotFoundException exc) {
+		return ResponseEntity.notFound().build();
 	}
 
 }
